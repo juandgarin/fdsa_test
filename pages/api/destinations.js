4adc1bc1edda22c489bf978712d2472cf7c37a34
Juan Diego Garin <juand.garin@gmail.com>
2022-02-24 17:01:04 +0100

    13 attempt

--- pages/api/destinations.js
+++ pages/api/destinations.js
@@ -1,15 +1,9 @@
-
-import NextCors from 'nextjs-cors';
+ 
 import {addDestination, destinations} from '../../data/db.js'
 const { faker } = require('@faker-js/faker');
 export default async function handler(req, res) {
 
-  await NextCors(req, res, {
-    // Options
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    origin: '*',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
- });
+ 
   if(req.method === 'POST'){
  
     addDestination({
